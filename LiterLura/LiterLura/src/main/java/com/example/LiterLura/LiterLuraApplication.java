35e9618e5c769a56e6fb09a8a623badf681d42d7
Jeremy Duran <88753503+jado21@users.noreply.github.com>
2026-02-04 16:35:02 -0500

    Add files via upload

---
+++ LiterLura/LiterLura/src/main/java/com/example/LiterLura/LiterLuraApplication.java
@@ -0,0 +1,13 @@
+package com.example.LiterLura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterLuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterLuraApplication.class, args);
+	}
+
+}
---
+++ LiterLura/LiterLura/src/test/java/com/example/LiterLura/LiterLuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LiterLura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterLuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
